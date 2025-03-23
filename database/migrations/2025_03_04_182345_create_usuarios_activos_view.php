b1044191c9d4e90921f404724892f2347dc99d80
GonzaaaXD <gonzalouribe49@gmail.com>
2025-03-22 19:13:49 -0600

    n

--- database/migrations/2025_03_04_182345_create_usuarios_activos_view.php
+++ database/migrations/2025_03_04_182345_create_usuarios_activos_view.php
@@ -10,7 +10,22 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::statement("CREATE VIEW `usuarios_activos` AS select `noticias`.`usuarios`.`id` AS `id`,`noticias`.`usuarios`.`persona_id` AS `persona_id`,`noticias`.`usuarios`.`tipo_usuario_id` AS `tipo_usuario_id`,`noticias`.`usuarios`.`creado_por_superadmin_id` AS `creado_por_superadmin_id`,`noticias`.`usuarios`.`status_id` AS `status_id`,`noticias`.`usuarios`.`email` AS `email`,`noticias`.`usuarios`.`salt` AS `salt`,`noticias`.`usuarios`.`password` AS `password`,`noticias`.`usuarios`.`ultima_autenticacion` AS `ultima_autenticacion`,`noticias`.`usuarios`.`bloqueado` AS `bloqueado`,`noticias`.`usuarios`.`intentos_fallidos_contraseña` AS `intentos_fallidos_contraseña`,`noticias`.`usuarios`.`eliminado` AS `eliminado`,`noticias`.`usuarios`.`fecha_eliminacion` AS `fecha_eliminacion`,`noticias`.`usuarios`.`eliminado_por` AS `eliminado_por` from `noticias`.`usuarios` where `noticias`.`usuarios`.`eliminado` = 0");
+        DB::statement("CREATE VIEW `usuarios_activos` 
+        AS select `noticias`.`usuarios`.`id` 
+        AS `id`,`noticias`.`usuarios`.`persona_id` 
+        AS `persona_id`,`noticias`.`usuarios`.`tipo_usuario_id` 
+        AS `tipo_usuario_id`,`noticias`.`usuarios`.`creado_por_superadmin_id` 
+        AS `creado_por_superadmin_id`,`noticias`.`usuarios`.`status_id` 
+        AS `status_id`,`noticias`.`usuarios`.`email` 
+        AS `email`,`noticias`.`usuarios`.`salt` 
+        AS `salt`,`noticias`.`usuarios`.`password` 
+        AS `password`,`noticias`.`usuarios`.`ultima_autenticacion` 
+        AS `ultima_autenticacion`,`noticias`.`usuarios`.`bloqueado`
+        AS `bloqueado`,`noticias`.`usuarios`.`intentos_fallidos_contraseña` 
+        AS `intentos_fallidos_contraseña`,`noticias`.`usuarios`.`eliminado` 
+        AS `eliminado`,`noticias`.`usuarios`.`fecha_eliminacion` 
+        AS `fecha_eliminacion`,`noticias`.`usuarios`.`eliminado_por` 
+        AS `eliminado_por` from `noticias`.`usuarios` where `noticias`.`usuarios`.`eliminado` = 0");
     }
 
     /**
