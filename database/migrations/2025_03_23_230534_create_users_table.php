30c5479e36d77c90ea6f28ce4451a2e0f803f7d3
GonzaaaXD <gonzalouribe49@gmail.com>
2025-03-24 10:54:11 -0600

    Update 2025_03_23_230534_create_users_table.php

--- database/migrations/2025_03_23_230534_create_users_table.php
+++ database/migrations/2025_03_23_230534_create_users_table.php
@@ -13,9 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password', 32);
             $table->rememberToken();
             $table->timestamps();
         });
